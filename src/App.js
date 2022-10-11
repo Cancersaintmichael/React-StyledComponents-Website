6ca186e6174948495637d5c65ce8555b473d369f
Cancersaintmichael <113973270+Cancersaintmichael@users.noreply.github.com>
2022-10-11 11:36:50 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+
+import { Header } from './components/styled/Header.styled';
 
 export default function App() {
   return (
     <div>
+      <Header>rertert</Header>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
